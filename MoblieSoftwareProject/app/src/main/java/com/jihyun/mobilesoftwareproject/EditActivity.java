b161197ccc7934dc40d600dee069b29a4dd64ac6
jihyunniiii <jita1125@naver.com>
2022-11-24 01:17:35 +0900

    레이아웃 수정 + 저장 버튼 일부 구현
    저장 버튼 누를 경우 Main Activity로 전환 시 날짜 연동되는 기능 미구현

--- MoblieSoftwareProject/app/src/main/java/com/jihyun/mobilesoftwareproject/EditActivity.java
+++ MoblieSoftwareProject/app/src/main/java/com/jihyun/mobilesoftwareproject/EditActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,5 +19,15 @@ public class EditActivity extends AppCompatActivity {
         Intent intent = getIntent();
         curr = intent.getStringExtra("select_date");
         Curr_date.setText(curr + " 식단 추가");
+
+        ImageButton input_button = findViewById(R.id.save_button);
+        input_button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(EditActivity.this, MainActivity.class);
+                TextView Curr_date = findViewById(R.id.Curr_date);
+                intent.putExtra("now_date", Curr_date.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
