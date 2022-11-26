70f1f198bd0bd05aba713993c1ef46d1c5ff7682
jihyunniiii <jita1125@naver.com>
2022-11-26 22:52:24 +0900

    EditActivity 식사 유형 입력 구현

--- MoblieSoftwareProject/app/src/main/java/com/jihyun/mobilesoftwareproject/EditActivity.java
+++ MoblieSoftwareProject/app/src/main/java/com/jihyun/mobilesoftwareproject/EditActivity.java
@@ -1,15 +1,23 @@
 package com.jihyun.mobilesoftwareproject;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
+    private String[] category = {"Breakfast", "Lunch", "Dinner", "Snack"};
+    private TextView categoryText;
+    private AlertDialog categoryDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +29,7 @@ public class EditActivity extends AppCompatActivity {
         Curr_date.setText(curr + " 식단 추가");
 
         ImageButton save_button = findViewById(R.id.save_button);
-        save_button.setOnClickListener(new View.OnClickListener(){
+        save_button.setOnClickListener(new OnClickListener(){
             public void onClick(View view){
                 Intent intent = new Intent(EditActivity.this, DetailActivity.class);
                 //Intent intent = new Intent(EditActivity.this, MainActivity.class);
@@ -30,5 +38,25 @@ public class EditActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        categoryText = (TextView) findViewById(R.id.textView5);
+        categoryText.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                categoryDialog.show();
+            }
+        });
+
+        categoryDialog = new AlertDialog.Builder(EditActivity.this)
+                .setItems(category, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        categoryText.setText("  " + category[i]);
+                    }
+                })
+                .setTitle("식사 유형을 선택해주세요")
+                .setPositiveButton("확인",null)
+                .setNegativeButton("취소",null)
+                .create();
     }
 }
